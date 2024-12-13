4a57e1a581a6bb4b0f52dead89d265442b659bf8
enzo_vargas <enzo.vargas@orsayphysics.com>
2024-12-13 15:42:33 +0100

    logo

--- track_air/lib/landingPage.dart
+++ track_air/lib/landingPage.dart
@@ -2,8 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:track_air/ParametrePage.dart';
 import 'package:track_air/StartPage.dart';
 
-class landingPage extends StatelessWidget {
-  const landingPage({super.key});
+class LandingPage extends StatelessWidget {
+  const LandingPage({super.key});
 
   @override
   Widget build(BuildContext context) {
@@ -15,23 +15,55 @@ class landingPage extends StatelessWidget {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: [
+            // Placeholder for the logo
+            Container(
+              margin: const EdgeInsets.only(bottom: 40),
+              child: Image.asset(
+                'assets/logotrackair.png', // Replace with your image path
+                height: 100, // Adjust height as needed
+                width: 100,
+                ),
+              ),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.green,
+                minimumSize: const Size(200, 50),
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const Startpage()),
                 );
               },
-              child: const Text('Go to Page One'),
+              child: const Text('Démarrer une partie'),
             ),
+            const SizedBox(height: 20),
             ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: const Size(200, 50),
+              ),
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const Startpage()),
+                );
+              },
+              child: const Text('Stats / Historique'),
+            ),
+            const SizedBox(height: 20),
+            ElevatedButton(
+              style: ElevatedButton.styleFrom(
+                backgroundColor: Colors.blue,
+                minimumSize: const Size(200, 50),
+              ),
               onPressed: () {
                 Navigator.push(
                   context,
                   MaterialPageRoute(builder: (context) => const ParametrePage()),
                 );
               },
-              child: const Text('Go to Page Three'),
+              child: const Text('Settings'),
             ),
           ],
         ),
